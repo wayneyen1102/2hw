d5835a8ce2ff199aeb3f9daab1a66aa57c575ab0
wayneyen1102 <86998504+wayneyen1102@users.noreply.github.com>
2021-07-07 09:29:32 +0800

    Add files via upload

---
+++ untitled04.py
@@ -0,0 +1,11 @@
+x = input('輸入數學考試成績')
+y = input('輸入英文考試成績')
+x = int(x)
+y = int(y)
+if x >= 90 and y >= 90:
+    print('有獎勵')
+elif x < 60 or y < 60:
+    print('要加油')
+elif x < 60 and y < 60
+    print('要處罰')
+    
